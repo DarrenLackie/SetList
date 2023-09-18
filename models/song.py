0dbe9932b706e52e4c45a18803ae6bb622b7b95d
darren lackie <lackiewwpj@gmail.com>
2023-09-18 21:46:58 +0100

    started css styling. created a cool menu dropdown button. Need to work on positioning.

--- models/song.py
+++ models/song.py
@@ -7,7 +7,10 @@ class Song(db.Model):
     title = db.Column(db.String(255))
     album = db.Column(db.String(255))
     running_time = db.Column(db.Integer)
+    # azlyrics_artist = db.Column(db.String(255))
+    # azlyrics_song = db.Column(db.String(255))
     setlists = db.relationship("SetListItem", backref="song")
 
     def __repr__(self):
         return f"<Song {self.id}: Title {self.title}: Album {self.album}: Song Length {self.running_time}>"
+    
