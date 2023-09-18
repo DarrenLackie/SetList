e8a70ce09fc94d1271009c0cc773bd52c5a856d3
darren lackie <lackiewwpj@gmail.com>
2023-09-18 11:58:42 +0100

    fixed setlist controller, can now add multiple songs to a setlist and it displays

--- controller/setlist_controller.py
+++ controller/setlist_controller.py
@@ -18,18 +18,20 @@ def new_setlist():
     gigs = Gig.query.all()
     return render_template("setlists/new.jinja", songs=songs, gigs=gigs)
 
-# @setlists_blueprint.route("/setlists",  methods=['POST'])
-# def create_setlist():
-#     song_ids = request.form.getlist('song_id[]')
-#     gig_id = request.form.get('gig_id')
-#     for song_id in song_ids:
-#         setlist = Setlist(song=song_id, gig=gig_id)
-#         db.session.add(setlist)
-#     db.session.commit()
-#     return redirect('/setlists')
+@setlists_blueprint.route("/setlists",  methods=['POST'])
+def create_setlist():
+    song_ids = request.form.getlist('song_id')
+    # print(f"song_ids are {song_ids}")
+    gig_id = request.form.get('gig_id')
+    for song_id in song_ids:
+        setlist = Setlist(song_id=int(song_id), gig_id=gig_id)
+        db.session.add(setlist)
+    
+    db.session.commit()
+    return redirect('/setlists')
 
-# @setlists_blueprint.route("/setlists/<id>/delete", methods=['POST'])
-# def delete_setlist(id):
-#     Setlist.query.filter_by(id = id).delete()
-#     db.session.commit()
-#     return redirect('/setlists')
+@setlists_blueprint.route("/setlists/<id>/delete", methods=['POST'])
+def delete_setlist(id):
+    Setlist.query.filter_by(id = id).delete()
+    db.session.commit()
+    return redirect('/setlists')
